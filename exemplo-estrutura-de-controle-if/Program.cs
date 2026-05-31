924901c5f934dbd9058dd027768f301ee6549b40
Raphael Fratel <raphael.fratel@gmail.com>
2026-05-31 09:55:20 -0300

    Adicona exemplo de estrutura de controle if

---
+++ exemplo-estrutura-de-controle-if/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+  public static void Main()
+  {
+    Console.WriteLine("Informe o raio de um círculo (deve ser um número inteiro)");
+    string? response = Console.ReadLine();
+    int radius = 0;
+    var canConvert = Int32.TryParse(response, out radius);
+
+    if (canConvert)
+    {
+      const double pi = 3.14159;
+
+
+      double circumference = pi * (2 * radius);
+
+      Console.WriteLine("A circunferencia de um circulo com o raio " + radius + " é igual a " + circumference);
+    }
+    else
+    {
+      Console.WriteLine("O texto digitado não é um número inteiro.");
+
+    }
+  }
+}
+
+//No código acima, o programa define se calcula e imprime a circunferência
+//de acordo com o valor da variável canConvert. 
+//Assim, conseguimos alterar a ordem na qual as instruções
+//são executadas de acordo com o valor informado pelo usuário.
