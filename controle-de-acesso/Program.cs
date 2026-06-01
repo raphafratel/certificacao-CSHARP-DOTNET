e1aa5fc80fa15712cc0fae65ae47fb7047158b89
Raphael Fratel <raphael.fratel@gmail.com>
2026-06-01 09:12:28 -0300

    Adiciona exec Controle de Acesso

---
+++ controle-de-acesso/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+  public static void Main()
+  {
+    int numberOfPeople = 0;
+    int numberOfChildren = 0;
+    int age = 99;
+
+    while (age > 0)
+    {
+      Console.WriteLine("Informe a idade da pessoa. Informe 0 para sair");
+      age = Convert.ToInt32(Console.ReadLine());
+      if (age == 0) break;
+      numberOfPeople++;
+      if (age < 18) numberOfChildren++;
+    }
+    double rateOfChildren = 0;
+
+    if (numberOfPeople > 0)
+    {
+      rateOfChildren = 100 * numberOfChildren / numberOfPeople;
+      Console.WriteLine("Existe " + rateOfChildren + " % de crianças");
+    }
+  }
+}
